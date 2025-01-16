cb155b61537d6887877c8ca5d0bd6d32142e151b
sep3r <sep3r@yahoo.com>
2025-01-17 00:53:38 +0330

    feat(backbone):added backbone of the project

---
+++ src/main/java/com/lobox/imdb_codechallenge_rest_ws/ImdbCodechallengeRestWsApplication.java
@@ -0,0 +1,13 @@
+package com.lobox.imdb_codechallenge_rest_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbCodechallengeRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbCodechallengeRestWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobox/imdb_codechallenge_rest_ws/ImdbCodechallengeRestWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobox.imdb_codechallenge_rest_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbCodechallengeRestWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
